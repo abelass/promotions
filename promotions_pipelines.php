d65b5920d8b707b0790953670bf510dcb951b3c1
Rainer <rainer@websimple.be>
2014-05-25 12:38:22 +0200

    début de l'essai pour afficher la promotion

--- promotions_pipelines.php
+++ promotions_pipelines.php
@@ -40,7 +40,7 @@ function promotions_reservation_evenement_donnees_details($flux){
 	
 	$date=date('Y-m-d H:i:s');
 	$sql=sql_select('*','spip_promotions','statut='.sql_quote('publie'));
-	
+
 		while ($data=sql_fetch($sql)){
 			if(
 				$details = charger_fonction('action', 'promotions/'.$data['type_promotion'], true) 
@@ -63,7 +63,9 @@ function promotions_reservation_evenement_donnees_details($flux){
 								'prix_promotion'=>$flux['data']['prix_ht']
 								)							
 							);
+						//On passe le nom de la table pour la pipeline post_insertion	
 						set_request('table',$flux['data']['table']);
+						
 						}
 			else set_request('donnees_promotion','');
 									
@@ -72,15 +74,50 @@ function promotions_reservation_evenement_donnees_details($flux){
 	return $flux;
 }
 
-//Enregistrement des dails de la promotion accordée
+//Enregistrement des détails de la promotion accordée
 
 function promotions_post_insertion($flux){
 	$table=$flux['args']['table'];
+	
 	if($donnees_promotion=_request('donnees_promotion') AND $table==_request('table')){
-		$donnees_promotion['id_objet']=$flux['args']['id_objet'];		
+		$promotions=_request('promotions')?_request('promotions'):array();
+		$donnees_promotion['id_objet']=$flux['args']['id_objet'];
+		
+		//On liste toutes
+		set_request('promotions',array_merge($promotions,array($donnees_promotion)));	
+
 		sql_insertq('spip_promotions_liens',$donnees_promotion);		
 	};
 
 return $flux;	
 }
+
+/*function promotions_formulaire_traiter($flux){
+	$form = $flux['args']['form'];
+	if ($form=='reservation' AND $promotions=_request('promotions')){
+		
+		$promotion=$promotions[0];
+		
+		$sql=sql_select(
+			'prix_original,titre',
+			'spip_promotions_liens,spip_promotions',
+			'id_objet='.$promotion['id_objet'].' AND objet='.sql_quote($promotion['objet']));
+		$i=0;
+		$titre=array();
+		while($data=sql_fetch($sql)){
+			$i++;			
+			if($i==1)$prix_original=$data['prix_original'];
+			$titre[]=extraire_multi($data['titre']);
+		};
+
+		
+		//Afficher la promotion
+		$replace=recuperer_fond('inclure/promotion_reservation',array('prix_original'=>$prix_original,'titre'=>implode(', ',$titre)));
+					echo $replace;
+		
+		$message_ok=str_replace('<tr class="total_ttc">', $replace.'<tr class="total_ttc">', $flux['data']['message_ok']);
+		$flux['data']['message_ok']=$message_ok;
+	}
+	return $flux;
+}*/
 ?>
