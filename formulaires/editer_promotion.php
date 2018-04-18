eaaf784e659464c581b186c6845a55d9e95cfba9
abelass <rainer@websimple.be>
2018-04-18 10:14:58 +0200

    unserialize plugins_applicables en chargean le formulaire

--- formulaires/editer_promotion.php
+++ formulaires/editer_promotion.php
@@ -62,8 +62,19 @@ function formulaires_editer_promotion_charger_dist($id_promotion = 'new', $retou
 	$valeurs = formulaires_editer_objet_charger('promotion', $id_promotion, '', $lier_trad, $retour, $config_fonc, $row, $hidden);
 
 	$type_promotion = _request('type_promotion') ? _request('type_promotion') : (isset($valeurs['type_promotion']) ? $valeurs['type_promotion'] : '');
+	$valeurs['non_cumulable'] = _request('non_cumulable')
+		? _request('non_cumulable') :
+		(isset($valeurs['non_cumulable'])
+				? unserialize($valeurs['non_cumulable'])
+				: ''
+		);
+	$valeurs['plugins_applicables'] = _request('plugins_applicables') ?
+		_request('plugins_applicables') :
+		(
+				isset($valeurs['plugins_applicables']) ? unserialize($valeurs['plugins_applicables']) : ''
+		);
 
-	$valeurs['non_cumulable'] = _request('non_cumulable') ? _request('non_cumulable') : isset($valeurs['non_cumulable']) ? unserialize($valeurs['non_cumulable']) : '';
+
 
 	$valeurs_promotion = $valeurs['valeurs_promotion'] = unserialize($valeurs['valeurs_promotion']);
 
